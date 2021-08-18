b7c503885cdb2413e9e4cfd839126ad8f400595b
hsm <huaism@126.com>
2021-08-18 16:46:13 +0800

    Initial commit

---
+++ providers/src/main/java/maven/NacosProviderDemoApplication.java
@@ -0,0 +1,27 @@
+package maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderDemoApplication.class, args);
+    }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{string}")
+//        @SentinelResource("hello")
+        public String echo(@PathVariable String string) throws InterruptedException {
+            Thread.sleep(1000L);
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+}

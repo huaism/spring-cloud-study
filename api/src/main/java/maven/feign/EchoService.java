964c439c0a408a7852654a2512cc94d5d0a68087
hsm <huaism@126.com>
2021-08-18 17:29:33 +0800

    add feign

---
+++ api/src/main/java/maven/feign/EchoService.java
@@ -0,0 +1,26 @@
+package maven.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "nacos-provider", fallback = EchoServiceFallback.class, configuration = FeignConfiguration.class)
+public interface EchoService {
+    @GetMapping(value = "/echo/{str}")
+    String echo(@PathVariable("str") String str);
+}
+
+class FeignConfiguration {
+    @Bean
+    public EchoServiceFallback echoServiceFallback() {
+        return new EchoServiceFallback();
+    }
+}
+
+class EchoServiceFallback implements EchoService {
+    @Override
+    public String echo(@PathVariable("str") String str) {
+        return "echo fallback";
+    }
+}
--- consumer/src/main/java/maven/NacosConsumerApp.java
+++ consumer/src/main/java/maven/NacosConsumerApp.java
@@ -1,5 +1,6 @@
 package maven;
 
+import maven.feign.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,11 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "maven")
 public class NacosConsumerApp {
+
+    @Autowired
+    private EchoService echoService;
 
     @RestController
     public class NacosController{
@@ -29,11 +35,14 @@ public class NacosConsumerApp {
 
         @GetMapping("/echo/app-name")
         public String echoAppName(){
-            //Access through the combination of LoadBalanceClient and RestTemplate
-            ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-provider");
-            String path = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
-            System.out.println("request path:" +path);
-            return restTemplate.getForObject(path,String.class);
+//            //Access through the combination of LoadBalanceClient and RestTemplate
+//            ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-provider");
+//            String path = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
+//            System.out.println("request path:" +path);
+//            return restTemplate.getForObject(path,String.class);
+
+            // fegin get
+            return echoService.echo(appName);
         }
 
     }
